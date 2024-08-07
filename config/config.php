aa1bf8c06c762ee16758bd57bceb986b05419d1d
Marcos Felipe <fenriz.felipe.95@gmail.com>
2024-08-06 21:11:14 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@
     $projectName = "Free Library";
 
     # Diretório auxiliar do projeto 
-    $projectFolder = "free-library";  
+    $projectFolder = "main";  
 
     # Url base (do navegador) do projeto 
     $baseUrl  = "http://".$_SERVER['SERVER_NAME']."/".$projectFolder;
